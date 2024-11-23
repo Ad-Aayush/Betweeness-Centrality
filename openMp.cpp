70755f4fe68bd109d2120f1e6b3f0d032b619f56
Ad-Aayush <aayushadlakha15@gmail.com>
2024-11-23 18:15:21 +0530

    Add working OpenMP.

--- openMp.cpp
+++ openMp.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>
-#include <omp.h>
+
+#include <atomic>
+#include <vector>
 using namespace std;
 
 struct Graph {
   int n;
-  std::vector<std::vector<int>> adj;
+  vector<vector<int>> adj;
 
-  Graph(int vertices) : n(vertices), adj(vertices, std::vector<int>()) {}
+  Graph(int vertices) : n(vertices), adj(vertices) {}
 
   void add_edge(int u, int v) {
     adj[u].push_back(v);
@@ -14,64 +16,82 @@ struct Graph {
   }
 };
 
-std::vector<double> OpenMpBrandes(Graph &G) {
-  int n = G.n;
-  std::vector<double> BC(n, 0.0);
+vector<double> OpenMpBrandes(Graph &G) {
+  const int n = G.n;
+  const int MAX_PHASE_SIZE = 1024;
+  const int MAX_SUCC_SIZE = 1024;
+  vector<double> BC(n, 0.0);
 
   for (int s = 0; s < n; ++s) {
-    std::vector<std::vector<int>> P(n);
-    std::vector<int> sigma(n, 0);
-    std::vector<int> d(n, -1);
+    vector<vector<int>> Succ(
+        n, vector<int>(MAX_SUCC_SIZE, -1));
+    vector<atomic<int>> Succ_size(n);  
+    vector<atomic<int>> sigma(n);       
+    vector<atomic<int>> d(n);            
+    vector<vector<int>> S(n, vector<int>(MAX_PHASE_SIZE));
+    vector<atomic<int>> S_size(n);  
+    vector<double> delta(n, 0.0);   
 
+    for (int i = 0; i < n; ++i) {
+      sigma[i] = 0;
+      d[i] = -1;
+      Succ_size[i] = 0;
+      delta[i] = 0.0;
+      S_size[i] = 0;
+    }
     sigma[s] = 1;
     d[s] = 0;
+    S[0][0] = s;
+    S_size[0] = 1;
 
     int phase = 0;
-    std::array<std::vector<int>, 100> S;  // We need some high number of phases
-    S[phase].push_back(s);
 
-    int cnt = 1;
+    while (S_size[phase] > 0) {
+      S_size[phase + 1] = 0;
 
-    while (cnt > 0) {
-      cnt = 0;
 #pragma omp parallel for
-      for (auto v : S[phase]) {
-#pragma omp for
+      for (int i = 0; i < S_size[phase].load(); ++i) {
+        int v = S[phase][i];
         for (int w : G.adj[v]) {
-          if (d[w] < 0) {
-            S[phase + 1].push_back(w);
-            cnt++;
-            d[w] = d[v] + 1;
+          int dw = -1;
+          if (d[w].compare_exchange_strong(dw, d[v] + 1)) {
+            int pos = S_size[phase + 1].fetch_add(1);
+            S[phase + 1][pos] = w;
           }
-          if (d[w] == d[v] + 1) {
-            sigma[w] += sigma[v];
-            P[w].push_back(v);
+          if (d[w].load() == d[v] + 1) {
+            sigma[w].fetch_add(sigma[v].load());
+            int pos = Succ_size[v].fetch_add(1);
+            if (pos < MAX_SUCC_SIZE) {
+              Succ[v][pos] = w;
+            }
           }
         }
-        phase = phase + 1;
       }
+      phase++;
     }
-    phase = phase - 1;
 
-    std::vector<double> delta(n, 0.0);
-
+    // Phase II: Back-propagation
     while (phase > 0) {
-      #pragma omp parallel for
-      for (int i = 0; i < S[phase].size(); ++i) {
+      phase--;
+#pragma omp parallel for
+      for (int i = 0; i < S_size[phase].load(); ++i) {
         int w = S[phase][i];
-        for (auto &v : P[w]) {
-          double c = ((double)sigma[v] / sigma[w]) * (1.0 + delta[w]);
-          #pragma omp atomic
-          delta[v] += c;
+        double dsw = 0.0;
+        double sw = sigma[w].load();
+
+        for (int j = 0; j < Succ_size[w].load(); ++j) {
+          int v = Succ[w][j];
+          dsw += (sw / sigma[v].load()) * (1.0 + delta[v]);
         }
+        delta[w] = dsw;
+
         if (w != s) {
-          #pragma omp atomic
-          BC[w] += delta[w];
+          BC[w] += dsw;
         }
       }
-      phase = phase - 1;
     }
   }
+
   return BC;
 }
 
@@ -84,12 +104,11 @@ int main() {
   G.add_edge(3, 4);
   G.add_edge(4, 5);
 
-  std::vector<double> BC = OpenMpBrandes(G);
+  vector<double> BC = OpenMpBrandes(G);
 
-  // Print the betweenness centrality scores
-  std::cout << "Betweenness Centrality Scores:\n";
+  cout << "Betweenness Centrality Scores:\n";
   for (int v = 0; v < BC.size(); ++v) {
-    std::cout << "Vertex " << v << ": " << BC[v] << "\n";
+    cout << "Vertex " << v << ": " << BC[v] << "\n";
   }
 
   return 0;
